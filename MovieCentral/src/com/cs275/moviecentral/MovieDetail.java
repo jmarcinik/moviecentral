b9aa301636c4e9a90238ada36030fd2139fa07da
Joel Marcinik <jdm329@drexel.edu>
2014-03-11 14:59:50 -0400

    Updated project files, added movie details activity

---
+++ MovieCentral/src/com/cs275/moviecentral/MovieDetail.java
@@ -0,0 +1,22 @@
+package com.cs275.moviecentral;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MovieDetail extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_movie_detail);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.movie_detail, menu);
+		return true;
+	}
+
+}
